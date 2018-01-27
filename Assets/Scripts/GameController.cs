dfa14ba9ae937f0fb83423d601d013e3d9bbad8e
Paul Fielek <paulfielek@gmail.com>
2018-01-27 12:20:32 -0500

    Made basic grid

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour {
+	public int gridX;
+	public int gridY;
+	public Node NodePrefab;
+	public float padding;
 
-	// Use this for initialization
+	private Node[,] nodes;
+
 	void Start () {
-		
+		SetupGame ();
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-		
+	}
+
+	void SetupGame () {
+		nodes = new Node[gridX, gridY];
+		for (int x = 0; x < gridX; x++) {
+			for (int y = 0; y < gridY; y++) {
+				Node newNode = Instantiate (NodePrefab);
+				newNode.transform.Translate (new Vector2 ((x - (gridX + (gridX - 1) * padding) / 2) + (x * padding), (y - (gridY + (gridY - 1) * padding) / 2) + (y * padding)));
+				nodes [x, y] = newNode;
+			}
+		}
 	}
 }
--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -3,14 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Node : MonoBehaviour {
-
-	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
 	}
 }
